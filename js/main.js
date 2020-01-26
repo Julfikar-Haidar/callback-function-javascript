72af44f51826a0533a32440366ad6162ec0dd3b6
julfikar <julfikar6262@gmail.com>
2020-01-27 00:04:20 +0600

    callback

---
+++ js/main.js
@@ -0,0 +1,67 @@
+var years = [1995,1965, 1950, 1980, 1990];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+        // console.log(arr[i]);
+
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+let sumYears = function () {
+    let totalAge = 0;
+    let count = 0;
+    let avg = 0;
+    arrayCalc(years, function (obj) {
+        totalAge = totalAge + obj;
+        count++;
+
+    });
+    console.log(totalAge);
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+// var lul = arrayCalc(ages, sumYears);
+
+
+console.log(ages);
+console.log(rates);
+console.log(fullAges);
+sumYears();
+
+
+// var a = [10,20,30,40]
+// var sum = 0
+// var avg = 0
+// function sumUP(a) {
+//    for (var i=0; i<a.length; i++){
+//       sum += a[i];
+//    }
+//    avg = (sum/a.length);
+// }
+// function display(callback) {
+//     callback(a);
+//     console.log("Average of array is " + avg);
+// }
+// display(sumUP);
---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// oop javascript
+// function is object
+// function person(){
+//     const firstName="Julfikar"
+//     console.log(firstName);
+// }
+// person();
+// // Its showing property and value behind the scene Javascript function is object
+// // console.dir(person);
+// // you can add some property and function
+// person.lastName = "Haidar"
+// console.log(person.lastName);
+
+// person.fullName= function(){
+//     console.log("Maruf Hasan");
+// }
+
+// person.fullName();
+// this key 
+// 'use strict'
+// function Person(){
+//     //  constructor function 
+//     this.firstName="Julfikar"
+//     this.lastName="Haidar"
+//     console.log(this);
+// }
+// // person();
+
+// const obj = new Person();
+// // console.log(typeof obj);
